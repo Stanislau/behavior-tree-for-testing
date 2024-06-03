c847e7b7ef0231859132640f89a93484f4108ca5
stanislau_kanaplianik <stanislau_kanaplianik@epam.com>
2024-06-03 17:22:06 +0300

    Basic solution structure together with gitignore.

---
+++ BehaviorTreeForTests/BehaviorTreeForTests.Tests/FooTests.cs
@@ -0,0 +1,19 @@
+﻿using BehaviorTreeForTests.Core;
+using NUnit.Framework;
+
+namespace BehaviorTreeForTests.Tests
+{
+    /*
+     * @todo #4:30m/Arch Setup automation pipeline to check pull request health.
+     */
+
+    [TestFixture]
+    public class FooTests
+    {
+        [Test]
+        public void Smoke()
+        {
+            Assert.That(new Foo().ToString(), Is.EqualTo("bar"));
+        }
+    }
+}
